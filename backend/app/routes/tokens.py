7f2ed08b11008516e1a4f84c98a7257cd068ce0a
Andrew Zixuan Chen <z5693435@ad.unsw.edu.au>
2026-04-21 04:26:50 +1000

    Revert "testing gst inc"
    This reverts commit 7bba77aa75021daee6842668a5ba681dfa0cbb44.

--- backend/app/routes/tokens.py
+++ backend/app/routes/tokens.py
@@ -65,15 +65,15 @@ def _send_invoice(customer_name, email, tokens, cost, api_token, gst):
                 },
                 "issueDate": today_str,
                 "dueDate": today_str,
-                "totalAmount": cost,
+                "totalAmount": cost * (1 + gst / 100),
                 "currency": "AUD",
                 "lines": [
                     {
                         "lineId": "1",
                         "description": f"{tokens} tokens",
                         "quantity": 1,
-                        "unitPrice": cost * (1 / (1+gst/100)),
-                        "lineTotal": cost * (1 / (1+gst/100)),
+                        "unitPrice": cost,
+                        "lineTotal": cost,
                     }
                 ],
                 "gstPercent": gst,
