40e0f40d41637b5cab26ef38a4ff27c8b25f989f
Eric Wong <z5692161@ad.unsw.edu.au>
2026-04-20 22:33:09 +1000

    UPDATE: Puttin access token param into user_client()

--- backend/app/routes/tokens.py
+++ backend/app/routes/tokens.py
@@ -51,7 +51,7 @@ def get_tokens():
     if error:
         return error
 
-    client = user_client()
+    client = user_client(access_token)
     try:
         account = _account_for_user(client, str(user.id))
     except APIError:
@@ -91,7 +91,7 @@ def buy_tokens():
     if tokens is None:
         return return_error("BAD_REQUEST", "Missing or invalid data: tokens required.")
 
-    client = user_client()
+    client = user_client(access_token)
     try:
         account = _account_for_user(client, str(user.id))
     except APIError:
@@ -156,7 +156,7 @@ def redeem_tokens():
     if tokens is None:
         return return_error("BAD_REQUEST", "Missing or invalid data: tokens required.")
 
-    client = user_client()
+    client = user_client(access_token)
     try:
         account = _account_for_user(client, str(user.id))
     except APIError:
