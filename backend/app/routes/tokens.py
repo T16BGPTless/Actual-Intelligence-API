8fd9cc633a0e3c8bb0b236d19acad15da5cd1a26
Eric Wong <z5692161@ad.unsw.edu.au>
2026-04-21 03:34:42 +1000

    UPDATE: Fix regex

--- backend/app/routes/tokens.py
+++ backend/app/routes/tokens.py
@@ -88,7 +88,7 @@ def _send_invoice(customer_name, email, tokens, cost, api_token, gst):
         )
 
         # Extract the invoice ID using regex
-        invoice_ids = re.findall(r"<cbc:ID>(.+)</cbc:ID>", resp.text)
+        invoice_ids = re.findall(r"<cbc:ID>(.+?)</cbc:ID>", resp.text)
         if invoice_ids:
             # Guarantee invoice_id is a string
             invoice_id = str(invoice_ids[0])
